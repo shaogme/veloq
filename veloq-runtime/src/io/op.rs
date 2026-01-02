d66bce002638aa228b50e98ee043fa0c61186b7f
韶光 <admin@gloryouth.com>
2026-01-02 11:31:28 +0800

    Remove deprecated macro for IO operations and streamline `IoResources` enum definition. This change enhances code clarity by eliminating unnecessary boilerplate, following the recent integration of the `veloq-macros` crate.

--- veloq-runtime/src/io/op.rs
+++ veloq-runtime/src/io/op.rs
@@ -37,27 +37,6 @@ impl IoFd {
     }
 }
 
-#[macro_export]
-macro_rules! for_all_io_ops {
-    ($mac:ident) => {
-        $mac!(
-            ReadFixed(ReadFixed),
-            WriteFixed(WriteFixed),
-            Send(Send),
-            Recv(Recv),
-            Timeout(Timeout),
-            Accept(Accept),
-            Connect(Connect),
-            SendTo(SendTo),
-            RecvFrom(RecvFrom),
-            Wakeup(Wakeup),
-            Open(Open),
-            Close(Close),
-            Fsync(Fsync),
-        );
-    };
-}
-
 macro_rules! define_io_resources_enum {
     ( $($Variant:ident($Inner:ty)),* $(,)? ) => {
         pub enum IoResources {
